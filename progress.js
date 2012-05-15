8b8356c50cba9baccc94cada68a22abf1049a715
mdix <dix@mediaman.de>
2012-05-15 16:35:33 +0200

    Removed initialization

--- progress.js
+++ progress.js
@@ -1,43 +1,67 @@
-var Progress = { bar: {} };
-Progress.bar = (function(config) {
-    if (typeof config === "undefined") {
-        console.log('Please give config, like: { width: "100%", height: "4px", id: "progress1" }');
+var Progress = {};
+
+Progress.fillUpBar = (function(config) {
+	var self = this;
+    if ("undefined" === typeof config) {
+        console.log('Please give config, like: { id: "progress1", autoRemove: true, removeTimeout: 2000 , backgroundSpeed: 50 }');
         return false;
     }
-    var outerDiv   = createOuterDiv();
-    var innerDiv   = createInnerDiv();
-
+    var outerDiv  = createOuterDiv();
+    var innerDiv  = createInnerDiv();
+    var intervals = [];
+    
     function createOuterDiv() {  
         var outerDiv = document.createElement('div');
             outerDiv.setAttribute('class', 'outerDiv');
             outerDiv.setAttribute('id', config.id);
-            outerDiv.style.width = config.width;
-            outerDiv.style.height = config.height;
         return outerDiv;
     }  
     
     function createInnerDiv() {
         var innerDiv = document.createElement('div');
             innerDiv.setAttribute('class', 'innerDiv');
-            innerDiv.style.height = config.height;
-            innerDiv.style.width = '0px';
         return innerDiv;
     }
     
     function update(percent) {
+    	percent = percent > 100 ? 100 : percent < 0 ? 0 : percent;
         var renderedBar = document.getElementById(config.id);
         var onePercentInPixel = 0.01 * renderedBar.offsetWidth;
-        innerDiv.style.width = percent * onePercentInPixel + 'px';
+        innerDiv.style.width  = percent * onePercentInPixel + 'px';
+        checkForAutoRemoval(percent);
     }
     
     function renderTo(element) {
         outerDiv.appendChild(innerDiv);
         element.appendChild(outerDiv);
+        animateBackground(); 
     }
     
-    function remove() {
+    function animateBackground() {
+    	if (!config.backgroundSpeed) {
+    		return;
+    	}
+    	var position = 0;
+        intervals['backgroundAnimation'] = window.setInterval(function() {
+        	if (config.backgroundSpeed < 0) {
+        		innerDiv.style.backgroundPosition = ++position + 'px'; 
+        	} else {
+        		innerDiv.style.backgroundPosition = --position + 'px';
+        	}
+        }, config.backgroundSpeed);
+    }
+    
+    function checkForAutoRemoval(percent) {
+    	if (100 !== percent || true !== config.autoRemove) {
+    		return;
+    	}
+    	self.markedForRemoval = !config.removeTimeout ? remove() : window.setTimeout(remove, config.removeTimeout);
+    }
+    
+    function remove(callback) {
         var renderedProgressBar = document.getElementById(config.id);
         renderedProgressBar.parentNode.removeChild(renderedProgressBar);
+        delete(self);
     }
     
     return {  
@@ -46,5 +70,3 @@ Progress.bar = (function(config) {
         remove:   remove
     };  
 });
-
-x = new Progress.bar({ width: "100%", height: "4px", id: "progress1" } );
--- test/testProgressJs.js
+++ test/testProgressJs.js
@@ -1,13 +1,22 @@
-describe('Progress.bar instantiated without config object', function() {
+describe('Progress.fillUpBar instantiated without config object', function() {
     it('returns false', function() {
-        expect(Progress.bar()).toBeFalsy();
+        expect(Progress.fillUpBar()).toBeFalsy();
     });
 });
 
-describe('Progress.bar instantiated with config object', function() {
-    var config      = { width: "100%", height: "4px", id: "progress1" };
-    var progressBar = Progress.bar(config);
+describe('Progress.fillUpBar general (with config: { width: "100%", height: "4px", id: "progress1" })', function() {
+	beforeEach(function() {
+		config      = { width: "100%", height: "4px", id: "progress1" };
+	    progressBar = Progress.fillUpBar(config);
+	    progressBar.renderTo(document.getElementById('testArea'));
+	    renderedProgressBar = document.getElementById(config.id);
+	});
 
+	afterEach(function() {
+		document.getElementById('testArea').innerHTML = "";
+		delete(progressBar);
+	});
+	
     it('returns a progress bar object', function() {
         expect(typeof progressBar).toEqual('object');
     });
@@ -17,8 +26,7 @@ describe('Progress.bar instantiated with config object', function() {
     });
     
     it('which, when called, appends the progressbar to the given element', function() {
-        progressBar.renderTo(document.getElementById('testArea'));
-        var testAreaChildren = testArea.children;
+        var testAreaChildren = document.getElementById('testArea').children;
         expect(testAreaChildren[0].getAttribute('class')).toEqual('outerDiv');
     });
         
@@ -26,11 +34,23 @@ describe('Progress.bar instantiated with config object', function() {
         expect(typeof progressBar.update).toEqual('function');
     });
     
-    it('which, when called, extends the width of the inner div element', function() {
-        var renderedProgressBar = document.getElementById(config.id);
-        expect(renderedProgressBar.getElementsByClassName('innerDiv')[0].offsetWidth).toEqual(0);
+    it('which, when called, changes the width of the inner div element', function() {
+        var initialWidth = renderedProgressBar.getElementsByClassName('innerDiv')[0].offsetWidth;
         progressBar.update(20);
-        expect(renderedProgressBar.getElementsByClassName('innerDiv')[0].offsetWidth).toNotEqual(0);
+        expect(renderedProgressBar.getElementsByClassName('innerDiv')[0].offsetWidth).toNotEqual(initialWidth);
+    });
+    
+    it('but not above 100 percent', function() {
+    	progressBar.update(100);
+    	var width = renderedProgressBar.getElementsByClassName('innerDiv')[0].offsetWidth;
+    	progressBar.update(1000);
+    	expect(renderedProgressBar.getElementsByClassName('innerDiv')[0].offsetWidth).toEqual(width);
+    });
+    
+    it('and not below 0 percent', function() {
+    	progressBar.update(50);
+    	progressBar.update(-100);
+    	expect(renderedProgressBar.getElementsByClassName('innerDiv')[0].offsetWidth).toEqual(0);
     });
     
     it('which has remove method', function() {
@@ -38,8 +58,55 @@ describe('Progress.bar instantiated with config object', function() {
     });
     
     it('which, when called, removes the complete progress bar', function() {
-        var testArea = document.getElementById('testArea');
+        expect(document.getElementById('testArea').hasChildNodes()).toBeTruthy();
         progressBar.remove();
-        expect(testArea.hasChildNodes()).toBeFalsy();
+        expect(document.getElementById('testArea').hasChildNodes()).toBeFalsy();
     });
 });
+
+describe('Progress.fillUpBar autoRemove without timeout (with config: { width: "100%", height: "4px", id: "progress2", autoRemove: true })', function() {
+	beforeEach(function() {
+		config      = { width: "100%", height: "4px", id: "progress2", autoRemove: true };
+	    progressBar = Progress.fillUpBar(config);
+	    progressBar.renderTo(document.getElementById('testArea'));
+	});
+
+	afterEach(function() {
+		document.getElementById('testArea').innerHTML = "";
+		delete(progressBar);
+	});
+
+	it('if autoRemove is true, the bar is instantly removed once it reached 100%', function() {
+	    progressBar.update(10);
+	    expect(testArea.hasChildNodes()).toBeTruthy();
+	    progressBar.update(50);
+	    expect(testArea.hasChildNodes()).toBeTruthy();
+	    progressBar.update(100);
+	    expect(testArea.hasChildNodes()).toBeFalsy();
+	});
+});
+
+describe('Progress.fillUpBar autoRemove with timeout (with config: {width: "100%", height: "4px", id: "progress2", autoRemove: true, removeTimeout: 900})', function() {
+	beforeEach(function() {
+		config      = { width: "100%", height: "4px", id: "progress2", autoRemove: true, removeTimeout: 900 };
+	    progressBar = Progress.fillUpBar(config);
+	    progressBar.renderTo(document.getElementById('testArea'));
+	});
+
+	afterEach(function() {
+		document.getElementById('testArea').innerHTML = "";
+		delete(progressBar);
+	});
+
+	it('if autoRemove is true and removeTimeout is set, the bar is removed once it reached 100%, but after the timeout', function() {
+	    progressBar.update(100);
+	    expect(testArea.hasChildNodes()).toBeTruthy();
+	    
+		waits(1100);
+		
+		runs(function() {
+	        expect(testArea.hasChildNodes()).toBeFalsy();
+		});
+	});
+});
+
